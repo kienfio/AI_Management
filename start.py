76a7c5cb900b333cc46e2239b139ba1a2992c54f
kienfio <fioqing2513@gmail.com>
2025-06-10 17:52:41 +0800

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-import os
-from app import app
-
-if __name__ == "__main__":
-    # 获取端口
-    port = int(os.environ.get('PORT', 5000))
-    # 启动Web服务
-    app.run(host='0.0.0.0', port=port, debug=False)
-    print(f"应用正在监听端口 {port}") 
