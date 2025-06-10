eaa306b03e42f236ef7f1c773ae304cb5d5d59b5
kienfio <fioqing2513@gmail.com>
2025-06-10 16:09:42 +0800

    Update start.py

--- start.py
+++ start.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     # 获取端口
     port = int(os.environ.get('PORT', 5000))
     # 启动Web服务
-    app.run(host='0.0.0.0', port=port, debug=False) 
+    app.run(host='0.0.0.0', port=port) 
